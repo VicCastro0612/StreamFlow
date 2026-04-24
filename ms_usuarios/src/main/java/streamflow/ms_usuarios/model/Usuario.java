077b36e0f771acff3ad6248649fb471d70792a69
Claudio <cl.figueroac@duocuc.cl>
2026-04-24 16:03:34 -0400

    Creación model Usuario

---
+++ ms_usuarios/src/main/java/streamflow/ms_usuarios/model/Usuario.java
@@ -0,0 +1,33 @@
+package streamflow.ms_usuarios.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDateTime fechaRegistro;
+
+    @Column(nullable = false)
+    private Boolean activo;
+}
